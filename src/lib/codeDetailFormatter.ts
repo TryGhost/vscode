6fb200d1de1ab5e4350e8af45d712283bdaa6ae8
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-09-08 18:04:46 -0400

    code detail formatter

--- src/lib/codeDetailFormatter.ts
+++ src/lib/codeDetailFormatter.ts
@@ -1,35 +1,40 @@
-import * as vscode from 'vscode';
-import { HelperSchema } from './helperInterface';
+import * as vscode from "vscode";
+import { HelperSchema } from "./helperInterface";
 
 export function codeDetailFormatter(helper: HelperSchema) {
+  const { name, type, usage, definition, example, link, attributes, variables } = helper;
 
-    const {name, type, usage, definition, example, link} = helper;
-    
-    const markdown = new vscode.MarkdownString("", true);
-    markdown.supportHtml = true;
-    markdown.appendMarkdown(`(${type} helper) <b>${name}</b>: \`${usage}\`\n\n`);
-    markdown.appendMarkdown(`---------------------------------------------------------\n\n`);
-    markdown.appendMarkdown(`${definition}\n\n`);
-    
-    // if (properties) {
-    //     for (const prop of properties) {
-    //         markdown.appendMarkdown(`\n\n<b>${prop.name.charAt(0).toUpperCase() + prop.name.substring(1)}</b>: `);
-    //     markdown.appendMarkdown(makeHtmlList(prop.values));
-    //     }
-    // }
-    
-    markdown.appendMarkdown('\n\n<b>Example</b>\n\n');
+  const markdown = new vscode.MarkdownString("", true);
+  markdown.supportHtml = true;
+  markdown.appendMarkdown(`(${type} helper) **${name}**: \`${usage}\`\n\n`);
+  markdown.appendMarkdown('\n\n<hr>\n\n');
+  markdown.appendMarkdown(`${definition}\n\n`);
+  markdown.appendMarkdown(`\n\n$(link-external) [${link}](${link})\n\n`);
+  markdown.appendMarkdown('\n\n<hr>\n\n');
+  markdown.appendMarkdown("\n\n**Example**\n\n");
+  markdown.appendCodeblock(example, "handlebars");
+  markdown.appendMarkdown('\n\n<hr>\n\n');
 
-    markdown.appendCodeblock(example, 'handlebars');
+  if (attributes) {
+    markdown.appendMarkdown("**Attributes**");
+    for (const attribute of attributes) {
+      markdown.appendMarkdown(`\n- ${attribute}\n`);
+    }
+  }
   
-    markdown.appendMarkdown(`\n\n\n\n <b>$(link) Docs</b>: [${link}](${link})`);
-    
-    return markdown;
+  markdown.appendMarkdown('\n\n<hr>\n\n');
+
+  if (variables) {
+    markdown.appendMarkdown("**Variables**");
+    for (const attribute of variables) {
+      markdown.appendMarkdown(`\n- ${attribute}\n`);
+    }
+  }
+  return markdown;
 }
 
-const makeHtmlList = (els: string[]) => {
-    return formatter.format(els.map(el => "`" + el + "`"));
-};
-
 // @ts-ignore
-const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+const formatter = new Intl.ListFormat("en", {
+  style: "long",
+  type: "conjunction",
+});
